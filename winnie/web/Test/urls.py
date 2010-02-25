302d4e6425f1f8686ebff1db8da897b25ac24890
Joshua Kogut <joshua.kogut@gmail.com>
2010-02-25 14:41:59 -0800

    Removed old web

--- winnie/web/Test/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('potbotadmin.Test.views',        
-    (r'^stats$', 'stats'),
-    (r'^nick/(?P<nick>[a-z0-9A-Z\.\-_]+)$', 'nick'),
-    (r'^(?P<channel>[a-z\.]+)$', 'index'),
-    (r'^(?P<channel>[a-z\.]+)/since/(?P<hash>[a-z0-9]+)/$', 'since'),
-)
