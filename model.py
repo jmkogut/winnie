fe1f9c6694ad0c063c30cc707d02fcad18b50d25
Joshua Mikkael <dottru@gmail.com>
2014-01-25 04:57:11 -0500

    early model

--- model.py
+++ model.py
@@ -1,18 +1,44 @@
 # http://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/
-# ^ src
+# ^ src 
+# http://docs.sqlalchemy.org/en/rel_0_9/orm/tutorial.html#connecting
+# /////////// GUIDES ///////////////////////////
 
 from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import *
 
-metadata = MetaData()
+engine = create_engine('sqlite:///winnie.sqlite', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
-users = Table(
-    'users', metadata,
-    Column('id',       Integer, primary_key=True),
-    Column('username', String(512), nullable=False),
-    Column('mask',     String(512), nullable=True)
-)
+def Init():
+    Base.metadata.create_all(engine)
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///')
-    metadata.create_all(engine)
-    conn = engine.connect() 
+def Save(item, ses=False):
+    if ses == False:
+        ses = Session()
+
+    ses.add(item)
+    ses.commit()
+    return ses
+
+# //////////////////////
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    nick = Column(String)
+    mask = Column(String)
+
+    def __repr__(s):
+        return '<User %s>' % (s.nick,)
+
+def find_nick(nick, s=False):
+    if s == False: s = Session()
+    try:
+        return s.query(User.nick).filter(User.nick == nick).first()
+    except NoResultFound, e:
+        return False
+
